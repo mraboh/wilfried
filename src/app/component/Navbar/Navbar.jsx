894e873847c83115ea86c3eafc4f583cf01f309e
wilfried <josephaboh29@gmail.com>
2024-09-16 13:02:47 +0200

    done

--- src/app/component/Navbar/Navbar.jsx
+++ src/app/component/Navbar/Navbar.jsx
@@ -1,57 +1,33 @@
-"use client";
+'use client';  // Make sure this component is treated as a Client Component
 import React from "react";
 import styles from "./Navbar.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import { FaShoppingCart } from "react-icons/fa";
 
 const links = [
-    { name: "Maison", href: "/Maison" },
+  { name: "Maison", href: "/Maison" },
   { name: "Services", href: "/Services" },
-  { name: "Contact", href: "/contact" },
+  { name: "Contact", href: "/Contact" },
   { name: "Dashboard", href: "/Dashboard" },
 ];
 
 const Navbar = () => {
-  <div className={styles.container}>
-    <div className={styles.linksContainer}>
-      {links.map((link, index) => (
-        <Link href={link.href} className={styles.links} key={index}>
-          {link.name}
-        </Link>
-      ))}
-    </div>
-  </div>;
   return (
     <div className={styles.container}>
-      <Link href={'Maison'}>
-      
-      <div className={styles.logo}>
-        <Image src="/img1.png"  width={160} height={150}/>
-      </div>
+      <Link href="/Maison">
+        <div className={styles.logo}>
+          {/* Add alt attribute for accessibility */}
+          <Image src="/img1.png" width={160} height={150} alt="Maison Logo" />
+        </div>
       </Link>
       <div className={styles.linksContainer}>
-        
-      <Link href={"Maison"} className={styles.link}>
-        Maison{" "}
-        </Link>
-        <Link href={"Services"} className={styles.link}>
-        Services{" "}
-        </Link>
-      
-     
-        <Link href={"/Contact"} className={styles.link}>
-          Contact{" "}
-        </Link>
-        <Link href={"Dashboard"} className={styles.link}>
-        Dashboard{" "}
-        </Link>
+        {links.map((link, index) => (
+          <Link href={link.href} className={styles.link} key={index}>
+            {link.name}
+          </Link>
+        ))}
       </div>
-
-    
-      
     </div>
-    
   );
 };
 
