778ffe64376826ed5801e386e7329dedd3ccd908
wilfried <josephaboh29@gmail.com>
2024-09-16 12:42:46 +0200

    done

--- src/app/component/Footer/Footer.jsx
+++ src/app/component/Footer/Footer.jsx
@@ -7,23 +7,21 @@ export default function Footer() {
   return (
     <>
       <div className={styles.container}>
-      <Link href={'Maison'}>
-        <div className={styles.logo}>
-          <Image src="/img1.png" width={200} height={250} />
-      
-        </div>
+        <Link href={"Maison"}>
+          <div className={styles.logo}>
+            <Image src="/img1.png" width={200} height={250} />
+          </div>
         </Link>
         <h1 className={styles.h2}>
           QUI SOMMES-NOUS? <br />
-          Le Site de l&apos;Événementiel met <br /> tout son savoir faire pour vous
-          dénicher le lieu idéal pour <br />
+          Le Site de l&apos;Événementiel met <br /> tout son savoir faire pour
+          vous dénicher le lieu idéal pour <br />
           vos événements, et ce quelqu&apos;en soit la typologie. <br /> OÙ
           SOMMES-NOUS? <br />
-          C&apos;est principalement à Douala que nous travaillons : nous recensons
-          aujourd&apos;hui plus de 1000 adresses Camerounaises. <br /> - À propos de
-          nous <br /> - Notre Politique RSE <br /> - Nos salles
+          C&apos;est principalement à Douala que nous travaillons : nous
+          recensons aujourd&apos;hui plus de 1000 adresses Camerounaises. <br />{" "}
+          - À propos de nous <br /> - Notre Politique RSE <br /> - Nos salles
         </h1>
-
 
         <div className={styles.lineText}>
           <div>
@@ -37,19 +35,16 @@ export default function Footer() {
             />
           </div>
           <h2>Contact:</h2>
-      <p>© 2024, Tech2 etc - CAMEROUN Evenementiel Template!</p>
-      <ul>
-        <li>  - Addresse: 512 New deido, Rue du marche,Douala </li>
-        <li>- Telephone: +237 694 25 58 99 /(+237) 672 69 84 83</li>
-        <li>  - Horaires: 10:00 - 20:00, Lun - Sam</li>
-        <div className={styles.btns}>
+          <p>© 2024, Tech2 etc - CAMEROUN Evenementiel Template!</p>
+          <ul>
+            <li> - Addresse: 512 New deido, Rue du marche,Douala </li>
+            <li>- Telephone: +237 694 25 58 99 /(+237) 672 69 84 83</li>
+            <li> - Horaires: 10:00 - 20:00, Lun - Sam</li>
+            <div className={styles.btns}>
               <button>App Store</button>
               <button className={styles.width}>Google Play</button>
-            </div> 
-
-      </ul>
-     
-         
+            </div>
+          </ul>
         </div>
       </div>
     </>
